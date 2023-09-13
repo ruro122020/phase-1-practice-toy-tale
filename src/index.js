79a019880466fb08cba88881519d022ff9558d6b
Ruth <ruro122020@gmail.com>
2023-09-13 13:18:04 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -14,45 +14,97 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  function toyCollection (){
-    fetch('http://localhost:3000/toys')
-    .then(res => res.json())
-    .then((toys)=>{
-      const cardContainer = document.getElementById('toy-collection')
-      /*
-      <div class="card">
-       <h2>Woody</h2>
-       <img src="[toy_image_url]" class="toy-avatar" />
-       <p>4 Likes</p>
-       <button class="like-btn" id="[toy_id]">Like ❤️</button>
-      </div>
-      */
-      toys.forEach((toyObj)=>{
-        //create an h2, img, p, and a button
-         const card = document.createElement('div')
-         const h2 = document.createElement('h2')
-         const img = document.createElement('img')
-         const p = document.createElement('p')
-         const btn = document.createElement('button')
-         //set attributes to img and button
-         card.setAttribute('class', 'card')
-         btn.setAttribute('class', 'like-btn')
-         img.setAttribute('class', 'toy-avatar')
-         btn.id = toyObj.id
-         img.src = toyObj.image
-         //add text to h2, p, btn elements
-         
-         //append h2, img, p, and btn to card
-         card.appendChild(h2)
-         card.appendChild(img)
-         card.appendChild(p)
-         card.appendChild(btn)
-         cardContainer.appendChild(card)
-      })
-     
+document.addEventListener('DOMContentLoaded', () => {
 
+//Event listeners
+  document.querySelector('form').addEventListener('submit', handleSubmit)
+  
+//Event Handlers
+function handleSubmit(e){
+  e.preventDefault()
+  let objToy = {
+    name: e.target.name.value,
+    image: e.target.image.value,
+    likes: 0
+  }
+  renderToy(objToy)
+  addToy(objToy)
+}
+
+//DOM render functions
+  function renderToy(toy) {
+    const cardContainer = document.getElementById('toy-collection')
+    //create an h2, img, p, and a button
+    const card = document.createElement('div')
+    const h2 = document.createElement('h2')
+    const img = document.createElement('img')
+    const p = document.createElement('p')
+    const btn = document.createElement('button')
+    //set attributes to card img and button
+    card.setAttribute('class', 'card')
+    btn.setAttribute('class', 'like-btn')
+    img.setAttribute('class', 'toy-avatar')
+    btn.id = toy.id
+    img.src = toy.image
+    //add text to h2, p, btn elements
+    h2.textContent = toy.name
+    p.textContent = `${toy.likes} Likes`
+    btn.textContent = 'Like ❤️'
+    //append h2, img, p, and btn to card
+    card.appendChild(h2)
+    card.appendChild(img)
+    card.appendChild(p)
+    card.appendChild(btn)
+    cardContainer.appendChild(card)
+    //add likes to the card when like button is clicked
+    btn.addEventListener('click',() => {
+      let likeNumber = parseInt(p.textContent.slice(0, 1))
+      let newLikeNumber = likeNumber + 1
+      p.textContent = `${newLikeNumber} Likes`
+      const likeObj = {
+        likes: newLikeNumber
+      }
+      editToy(btn.id, likeObj)
     })
   }
-  toyCollection()
+
+
+  //Fetch requests
+  function getAllToys() {
+    fetch('http://localhost:3000/toys')
+      .then(res => res.json())
+      .then((toys) => toys.forEach(toy => renderToy(toy)))
+  }
+
+  function addToy(objToy) {
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(objToy)
+    })
+      .then(res => res.json())
+      .then(toy => console.log(toy))
+  }
+
+  function editToy(toyId, newNumberOfLikes){
+    fetch(`http://localhost:3000/toys/${toyId}`, {
+      method: 'PATCH',
+      headers:{
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body:JSON.stringify(newNumberOfLikes)
+    })
+    .then((res)=>res.json())
+    .then(data => console.log('data', data))
+  }
+
+  //initializ Render
+  function init() {
+    getAllToys()
+  }
+  init()
 })
